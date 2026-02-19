feabb13d5d730418865e3a3a44b5ea006bb17853
clara0904 <mclaradearaujop@aluno.uespi.br>
2026-02-19 15:24:57 -0300

    Criação do Menu

---
+++ src/app/shared/components/menu/menu.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css',
+})
+export class Menu {
+
+}
