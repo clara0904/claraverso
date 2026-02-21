9a430b208863f981da79647c8a7a156286e1b77a
clara0904 <mclaradearaujop@aluno.uespi.br>
2026-02-21 14:16:47 -0300

    Adição da rota da nova página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { Explorar } from './shared/components/explorar/explorar';
+import { DetalheResenha } from './shared/components/detalhe-resenha/detalhe-resenha';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
   path: 'explorar',
     component: Explorar
+  },
+  {
+    path: 'resenha/:id',
+    component: DetalheResenha
   }
 ];
